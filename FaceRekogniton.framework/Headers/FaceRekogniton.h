b72f9bdc9330d09fbbde34161fb62765b132a66a
BlackBart90 <jakopo_7@libero.it>
2024-12-05 18:04:31 +0100

    Add files via upload

---
+++ FaceRekogniton.framework/Headers/FaceRekogniton.h
@@ -0,0 +1,16 @@
+//
+//  FaceRekogniton.h
+//  FaceRekogniton
+//
+//  Created by Jacopo Pappalettera on 04/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaceRekogniton.
+FOUNDATION_EXPORT double FaceRekognitonVersionNumber;
+
+//! Project version string for FaceRekogniton.
+FOUNDATION_EXPORT const unsigned char FaceRekognitonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceRekogniton/PublicHeader.h>
